52a9e429e955325e44121a080b1716fd76d2cdd1
mihailgaberov <mihail.gaberov@gmail.com>
2018-07-14 21:30:10 +0200

    lab 1

--- final/exam-queries/helpers.js
+++ final/exam-queries/helpers.js
@@ -1,11 +1,36 @@
+/*
+        * TODO-lab1A
+        *
+        * LAB #1A: Implement the getCategories() method.
+        *
+        * Write an aggregation query on the "item" collection to return the
+        * total number of items in each category. The documents in the array
+        * output by your aggregation should contain fields for "_id" and "num".
+        *
+        * HINT: Test your mongodb query in the shell first before implementing
+        * it in JavaScript.
+        *
+        * In addition to the categories created by your aggregation query,
+        * include a document for category "All" in the array of categories
+        * passed to the callback. The "All" category should contain the total
+        * number of items across all categories as its value for "num". The
+        * most efficient way to calculate this value is to iterate through
+        * the array of categories produced by your aggregation query, summing
+        * counts of items in each category.
+        *
+        * Ensure categories are organized in alphabetical order before passing
+        * to the callback.
+        *
+        */
+
 db.item.aggregate([
     { $match: { category: { $exists: true } } },
     { $project: {
-        title: 1, category: 1
+        category: 1
     } },
     { $group: {
-        _id: { category: "$category" },
-        count: {
+        _id: { _id: "$category" },
+        num: {
             $sum: 1
         }
     } },
--- final/mongomart/items.js
+++ final/mongomart/items.js
@@ -27,38 +27,38 @@ function ItemDAO(database) {
     this.getCategories = function(callback) {
         "use strict";
 
-        /*
-        * TODO-lab1A
-        *
-        * LAB #1A: Implement the getCategories() method.
-        *
-        * Write an aggregation query on the "item" collection to return the
-        * total number of items in each category. The documents in the array
-        * output by your aggregation should contain fields for "_id" and "num".
-        *
-        * HINT: Test your mongodb query in the shell first before implementing
-        * it in JavaScript.
-        *
-        * In addition to the categories created by your aggregation query,
-        * include a document for category "All" in the array of categories
-        * passed to the callback. The "All" category should contain the total
-        * number of items across all categories as its value for "num". The
-        * most efficient way to calculate this value is to iterate through
-        * the array of categories produced by your aggregation query, summing
-        * counts of items in each category.
-        *
-        * Ensure categories are organized in alphabetical order before passing
-        * to the callback.
-        *
-        */
-
         var categories = [];
+        var cursor = this.db.collection('item').aggregate([
+            { $match: { category: { $exists: true } } },
+            { $project: {
+                category: 1
+            } },
+            { $group: {
+                _id: { _id: "$category" },
+                num: {
+                    $sum: 1
+                }
+            } },
+            { $sort: { _id: 1 } }
+        ] );
+
+        cursor.forEach(
+            function(doc) {
+              console.log(doc);
+            },
+            function(err) {
+                assert.equal(err, null);
+                //return this.db.close();
+            }
+        );
+
+
         var category = {
             _id: "All",
             num: 9999
         };
 
-        categories.push(category)
+        categories.push(category);
 
         // TODO-lab1A Replace all code above (in this method).
 
